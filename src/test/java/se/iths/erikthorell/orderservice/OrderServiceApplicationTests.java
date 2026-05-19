827a9410c84a563174fb2e26b298ed870fa7dc70
eritho91 <erik.thorell@iths.se>
2026-05-19 19:16:12 +0200

    Initial commit

---
+++ src/test/java/se/iths/erikthorell/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package se.iths.erikthorell.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
